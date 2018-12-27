96aee20a202a99162437bed47c7ec63b044cd1e9
DESKTOP-OR0SSCH\PhucDuc <phucducktpm@gmail.com>
2018-12-27 23:54:35 +0700

    Báo Cáo Xong

--- nap bao cáo Web/source/js/jqueryFormDangTin.js
+++
@@ -1,5 +0,0 @@
- /*$(document).ready(function() {
-     $('input[type="file"]').change(function () {
-         $(this).next().removeAttr('disabled');
-     }).next().attr('disable', 'disable');
- });*/
